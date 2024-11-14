76ad33ab010755e7e1ef36c94f9a4f5a01e93706
Asmiraj <93111361+Asmiraj@users.noreply.github.com>
2024-11-14 20:00:56 +0530

    SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class SudokuSolver {
+
+    public static final int SIZE = 9;
+    
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSafe(int[][] board, int row, int col, int num) {
+        return !usedInRow(board, row, num) && !usedInCol(board, col, num) && !usedInBox(board, row - row % 3, col - col % 3, num);
+    }
+
+    public static boolean usedInRow(int[][] board, int row, int num) {
+        for (int col = 0; col < SIZE; col++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean usedInCol(int[][] board, int col, int num) {
+        for (int row = 0; row < SIZE; row++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean usedInBox(int[][] board, int startRow, int startCol, int num) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row + startRow][col + startCol] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] board = new int[SIZE][SIZE];
+
+        System.out.println("Enter the Sudoku puzzle (use 0 for empty cells): ");
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+
+        if (solveSudoku(board)) {
+            System.out.println("Solved Sudoku:");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+
+        scanner.close();
+    }
+}
